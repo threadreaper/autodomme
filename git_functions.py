19f4aca47cc037182dfec3e6621144bd77a3b948
threadreaper <threadreaper@gmail.com>
2021-03-04 19:39:41 -0600

    Still trying to figure out git from python

--- git_functions.py
+++ git_functions.py
@@ -3,5 +3,5 @@ import os
 
 repo = Repo.init(os.getcwd())
 # repo.create_remote('origin', 'https://github.com/threadreaper/autodomme')
-repo.git.checkout((repo.remote().refs))
+# repo.git.checkout((repo.remote().refs))
 print(repo.active_branch)
