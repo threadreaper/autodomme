4c27473f0fd56dd1e5740b4b9a9760258408e7c6
threadreaper <threadreaper@gmail.com>
2021-03-05 01:17:11 -0600

    gitpython sucks.   sh is awesome

--- git_functions.py
+++ git_functions.py
@@ -1,12 +1,15 @@
 import os
-import sh
+from sh import git
 
-
+add_remote = git.bake('remote', 'add', 'origin',
+                      'https://github.com/threadreaper/autodomme.git')
 if '.git' not in os.listdir(os.getcwd()):
-    sh.git('init')
-    sh.git('remote', 'add', 'origin', 'https://github.com/threadreaper/autod,omme.git')
-    sh.git.checkout('new_client')
+    git('init')
+    add_remote()
+    git.checkout('new_client')
 else:
-    remote = sh.git('remote')
+    remote = git('remote')
     if remote.exit_code != 0:
-        sh.git('remote', 'add', 'origin', 'https://github.com/threadreaper/autodomme.git')
+        add_remote()
+
+git('pull')
