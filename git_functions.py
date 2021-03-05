3772406c783537c207066c53b0309e69c963c5e6
threadreaper <threadreaper@gmail.com>
2021-03-04 21:25:26 -0600

    More git work

--- git_functions.py
+++ git_functions.py
@@ -1,7 +1,13 @@
 from git import Repo
 import os
 
-repo = Repo.init(os.getcwd())
-# repo.create_remote('origin', 'https://github.com/threadreaper/autodomme')
-# repo.git.checkout((repo.remote().refs))
-print(repo.active_branch)
+
+if '.git' not in os.listdir(os.getcwd()):
+    repo = Repo.init(os.getcwd())
+    repo.create_remote('origin', 'https://github.com/threadreaper/autodomme')
+    repo.git.checkout('v0.0.2')
+else:
+    repo = Repo.init(os.getcwd())
+
+if repo.remotes[0].exists():
+    repo.remotes[0].fetch()
