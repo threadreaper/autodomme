69ce6d56f38501c62c87b882f38095584d616b62
threadreaper <threadreaper@gmail.com>
2021-03-05 13:55:11 -0600

    Output processing added for automatic updates

--- git_functions.py
+++ git_functions.py
@@ -2,8 +2,13 @@ import os
 from sh import git
 
 
+def process_output(line):
+    print(line)
+
+    
 def auto_update():
     """Updates the application from a github repo."""
+    print('UPDATING')
     add_remote = git.bake('remote', 'add', 'origin',
                           'https://github.com/threadreaper/autodomme.git')
 
@@ -13,4 +18,7 @@ def auto_update():
     elif git('remote').exit_code != 0:
         add_remote()
 
-    git('pull')
+    git('pull', _out=process_output)
+
+if __name__ == '__main__':
+    auto_update()
